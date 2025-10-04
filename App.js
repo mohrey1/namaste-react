a392ce099917fec1b75b4ebdf559cb565d7b7908
Mansi Ohrey <ohreym@gmail.com>
2025-10-04 06:44:39 +0530

    add header body to our app

--- App.js
+++ App.js
@@ -1,19 +1,57 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement("h1",{ id:"heading", xyz:"abc"},"hello world from react");
-//          const root = ReactDOM.createRoot(document.getElementById("root"));
-//          root.render(heading);
+  const Header = () => {
+    return <div className="header">
+      <div className="logo-container">
+          <img
+            className="logo"
+            src="https://images.pexels.com/photos/1633578/pexels-photo-1633578.jpeg?cs=srgb&dl=beef-bread-buns-1633578.jpg&fm=jpg"
+          />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  }
 
+  const RestaurantCard = () => {
+    return <div className="res-card" style={{backgroundColor: "f0f0f0"}}>
+        <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://myfoodstory.com/wp-content/uploads/2022/07/Chicken-Biryani-2-720x1080.jpg"
+        />
+        <h3>Mansi ki Biryani</h3>
+        <h4>Biryani, North Indian</h4>
+        <h4>4.4 Stars</h4>
+      </div>
+  }
 
-  // const parent = React.createElement("div",{id:"parent"}, React.createElement("div",{id:"child"},
-  //  [ React.createElement("h1",{},"i am a h1tag"), React.createElement("h2",{},"i am a h2tag")]
-  // ));
-
+  const Body = () => {
+    return <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+      </div>
+    </div>
+  }
 
   const AppLayout = () => {
   return <div className ="app">
-    Mansi
+    <Header/>
+    <Body/>
   </div>;
   }
   
