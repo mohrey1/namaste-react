c1bb5648117b5b869d157235682c20f8eed60db5
Mansi Ohrey <ohreym@gmail.com>
2025-10-04 06:00:09 +0530

    add applayout

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // const heading = React.createElement("h1",{ id:"heading", xyz:"abc"},"hello world from react");
 //          const root = ReactDOM.createRoot(document.getElementById("root"));
 //          root.render(heading);
 
-  const parent = React.createElement("div",{id:"parent"}, React.createElement("div",{id:"child"},
-   [ React.createElement("h1",{},"i am a h1tag"), React.createElement("h2",{},"i am a h2tag")]
-  ));
+
+  // const parent = React.createElement("div",{id:"parent"}, React.createElement("div",{id:"child"},
+  //  [ React.createElement("h1",{},"i am a h1tag"), React.createElement("h2",{},"i am a h2tag")]
+  // ));
+
+
+  const AppLayout = () => {
+  return <div className ="app">
+    Mansi
+  </div>;
+  }
   
   const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(parent);
+  root.render(<AppLayout/>);
+
+
+
   
